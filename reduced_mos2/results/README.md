dde8847848c4939b47d13a840c0872f7d6a4815f
Bastian Guggenberger <bastian.guggenberger@aon.at>
2025-08-27 16:31:51 +0200

    update folder structure

---
+++ reduced_mos2/results/README.md
@@ -0,0 +1,89 @@
+# Results
+The "reduced_mos2/results" - folder contains all result files from the graddesc runs, as well as a general and several special python scripts for formating these results.
+
+## types of runs and folder structure  
+In this project, several different settings and variations of gradient descent were experimented.  
+The result of each of these "runtypes" are each stored in a seperate folder:  
+
+- **energyorder_2.0:**  
+In this method, no gradient descent algorithm is used.  
+Instead, the cell hoppings are reduced in order of their hopping energy.  
+
+- **highiterations_2.0:**  
+Runs with these setting were done by the classic gradient descent algorithm "reduced_mos2/graddesc_mos2/mos2_graddesc.py", using 1200 iterations.  
+
+- **lowiterations_2.0:**  
+Runs with these setting were done by the classic gradient descent algorithm "reduced_mos2/graddesc_mos2/mos2_graddesc.py", using only 400 iterations.  
+
+- **multilevelruns_2.0**  
+These runs were produced by "reduced_mos2/graddesc_mos2/mos2_multilevel_graddesc.py", where the algorithm optimizes first and second degree neighbours distinctly. Still a total of 1200 iterations were used.
+
+- **bandgapruns_2.0:**  
+Runs with these setting were done by "reduced_mos2/graddesc_mos2/mos2_bandgap_graddesc.py", a version of the classig NGD algorithm "mos2_graddesc.py" where the algorithm prioritizes optimization in the bands near the bandgap.   
+
+- **tests**  
+Used for testruns, after changes in the scripts or folderstructure.  
+
+
+The types of results in each of these folders is described below.  
+"results.py" and other scripts in "special_results" are used to get formated results(pngs) out of the raw data.  
+A summary of some good results is stored in the folder "importantresults".  
+
+## unformated results
+the main programs of this project ("reduced_mos2/graddesc_mos2/mos2_graddesc.py", etc. ...) store the following raw data in .txt format, in the folders "reduced_mos2/results/-runtype-/":
+
+- **graddesc_finalx_run1234.txt:**  
+the final hoppingvector of the run stored as "x" (coordinates relative to the ideal hopping vector).  
+
+- **graddesc_x_run1234.txt:**  
+the hoppingvector of each iteration stored as "x" (coordinates relative to the ideal hopping vector).  
+
+- **graddesc_history_run1234.txt:**  
+the history of the gradient descent run - an overview over each iteration.  
+
+- **graddesc_params_run1234.txt**  
+the parameters (lambdas, iterations, ...) of the run.  
+
+## formated results
+
+### results.py
+The general-result-formating script "reduced_mos2/results/results.py" produces the following output in the folder "reduced_mos2/results/-runtype-/formated_results/":  
+
+- **graddesc_bands_run1234.png:**  
+Bandstructure plot of MOS2 cell with all hoppings vs MOS2 cell with hoppings redused by the given method:  
+!["graddesc_bands_run1234.png"](graddesc_bands_run1017.png)  
+
+- **graddesc_cellplot_run1234.png:**  
+A plot of the final reduced MOS2 cell:  
+!["graddesc_cellplot_run1234.png"](graddesc_cellplot_run1017.png)
+
+- **graddesc_xvector_run1234.png:**  
+A plot of the remaining hoppings, plotted as relative coordinates (x-vector):  
+!["graddesc_cellplot_run1234.png"](graddesc_xvector_run1017.png)  
+
+Also the information about the metric is stored in "reduced_mos2/results/-runtype-/mvsNanalysis/mvsN_run1234.txt".  
+
+### special_results/
+The folder "reduced_mos2/results/special_results/" contains several other scripts for plotting certain results:  
+
+- **mvsN/ :**  
+The scripts take the mvsN (metric vs remaining number of hoppings) information of each run from "reduced_mos2/results/-runtype-/mvsNanalysis/mvsN_run1234.txt" and stores a plot of this information in the folder "reduced_mos2/results/-runtype-/formated_results/".  
+It also plots a comparison of all mvsNs of all different methods and stores this in the "importantresults" folder.  
+
+- **historyanalysis/ :**  
+The scripts produce a plot of the mvsN information of a certain run over its iterations:  
+!["historyanalysis_seperatedlevels_run1234.png"](historyanalysis_seperatedlevels_run1007.png)  
+The plots are stored in the "importantresults" folder.  
+
+- **finalx/ :**  
+The scripts produce a plot of final x vector, seperated into degrees of order:  
+![graddesc_x_ordered1234.png](graddesc_x_ordered61.png)  
+The plots are stored in the "importantresults" folder.   
+
+- **bandstructures/bandgapanalysis.py :**  
+Prints the maximum error of the reduced model near the bandgap.  
+
+- **bandstructures/bandstructures.py :**  
+Creates a clean plot of a bandstructure for evaluating the performance of the project:  
+![graddesc_bands_redE_run1234.png](graddesc_bands_redE_run1007.png)
+The plot is stored in the "importantresults" folder.   
---
+++ reduced_mos2/results/results.py
@@ -0,0 +1,140 @@
+#Script for analyzing and plotting Results of Test runs of the Nesterov Gradient Descent PCA
+#Based on the script "pca_graddesc.py"
+
+import matplotlib.pyplot as plt
+import tbplas as tb
+from mos2class import mcell, msafebandstructure, mmetric, mxtohopvec
+
+import ast
+#-----------------------------------------------------
+#VARIABLES (IMPORTANT):
+pathsettings = "customized"
+
+if (pathsettings == "finalruns"):
+    path_runs = "finalruns/"
+    path_output = "finalruns/formated_results/"
+    path_mvsN_output = "finalruns/mvsNanalysis/"
+elif (pathsettings == "multilevelruns"):
+    path_runs = "multilevelruns_2.0/"
+    path_output = "multilevelruns_2.0/formated_results/"
+    path_mvsN_output = "multilevelruns_2.0/mvsNanalysis/"
+elif (pathsettings == "customized"):
+    path_runs = "tests/"
+    path_output = "tests/formated_results/"
+    path_mvsN_output = "tests/mvsNanalysis/"
+
+path_mvsN_energyorder_file = "/home/bastian/bachelorarbeit_Projekte/Projekte/reduced_mos2/resources/mvsN_energyorder/mvsN_energyorder.txt"
+
+
+IDset = [60000] #IDs of Runs to evaluate
+E_min = 0.1 #Must be same as in pca_graddesc.py
+
+
+#-----------------------------------------------------
+#MAIN:
+
+#IDEAL CELL:
+ideal_cell = mcell("Ideal",E_min)
+idealhops = ideal_cell.mhoppings
+#ideal_bandss = ideal_cell.mcalcbands()
+
+
+#-----------------------------------------------------
+#Functions:
+#-----------------------------------------------------
+
+def safe_Nvsm_graph(N_relative,m):
+    #Get N vs M Information of Energyreduced Cell:
+    fromlowetohighEfile = open(path_mvsN_energyorder_file,"r")
+    content = []
+    for line in fromlowetohighEfile:
+        content.append(line)
+    nvec = ast.literal_eval(content[0])
+    metrics = ast.literal_eval(content[1])
+
+    #Plotting:
+    plt.gca().invert_xaxis()
+    plt.axhline(y=0,color='grey')
+    plt.plot(nvec, metrics, color = "Red", label="Hoppings reduced in the order of increasing energies")
+    plt.scatter(N_relative,m,color="Green", label="Hoppings reduced with Nesterov Gradient Descent")
+    plt.ylim(top=500,bottom=0) #Important!
+    plt.xlabel("Number N of Hoppings / Total Number of Hoppings")
+    plt.ylabel("Error Metric m in eV")
+    plt.title("Error Metric m for varying number N of hoppings. \n lambda_0 = ")
+    plt.savefig(path_output+"graddesc_Nvsm_run"+str(ID)+".png")
+    plt.clf()
+
+
+
+#-----------------------------------------------------
+#Main:
+#-----------------------------------------------------
+
+for ID in IDset:
+
+    #Reading x from file
+    name = path_runs + "graddesc_finalx_run" + str(ID) + ".txt"
+    finalxfile = open(name,"r")
+    content = finalxfile.read()
+    x = ast.literal_eval(content)
+
+    #Calculating hopvec:
+    hopvec = mxtohopvec(x,idealhops)
+    hopvec = [hop for hop in hopvec if abs(hop[3])>E_min]
+
+    #Build the final cell:
+    reducedhopcell = mcell("hoppings reduced by Nesterov grad-desc", E_min)
+    reducedhopcell.mchangehops_tohopvec(hopvec)
+    N = reducedhopcell.mnhoppings
+
+    #Build the "true" cell:
+    truecell = mcell("original hoppings", 0)
+    total = truecell.mnhoppings
+    true_bands = truecell.mcalcbands()
+
+    #Plot N vs M Graph
+    m = mmetric(reducedhopcell,true_bands)
+    safe_Nvsm_graph(N/total,m)
+
+    #Plot Bandstructure Comparison with "true" cell
+    cellvector=[truecell,reducedhopcell]
+    filename= path_output + "graddesc_bands_run"+str(ID)
+    title = "Bandstructures of MoS2 cell with all hoppings \n vs MoS2 cell with reduced hoppings, \n using Nesterov-Gradient-Descent"
+    msafebandstructure(cellvector,filename,title)
+
+    #Plot Bandstructure Comparison with "true" and energyreduced cell
+    reducedenergycell = mcell("hoppings reduced in order of Energy",0)
+    allhops_array = reducedenergycell.mhoppingtable()[0]
+    allhops_array = allhops_array[allhops_array[:,6].argsort()[::-1]]
+    filtered_array = allhops_array[:N]
+    reducedenergycell.mchangehops_toarr(filtered_array)
+
+    cellvector=[truecell,reducedhopcell,reducedenergycell]
+    filename=path_output + "graddesc_bands_redE_run"+str(ID)
+    title = "Bandstructures of MoS2 cell with all hoppings \n vs MoS2 cell with Nesterov-GD-reduced hoppings \n vs MoS2 cell with reduced hoppings in order of Energy"
+    msafebandstructure(cellvector,filename,title)
+
+    #Plot x vector
+    indices = [i for i in range(len(x))]
+    plt.plot(indices,x,color = "Grey")
+    plt.scatter(indices,x,color="Red")
+    plt.xlabel("Index i")
+    plt.ylabel("Weight factor x[i]")
+    plt.title("Weight vector x")
+    plt.savefig(path_output + "graddesc_xvector_run"+str(ID)+".png")
+    plt.clf()
+
+    #Plot hopping terms, reduced hopping Cell
+    filename=path_output+"graddesc_cellplot_run"+str(ID)
+    reducedhopcell.mprimcell.plot(filename)
+
+    #Plot hopping terms, Comparison True Cell
+    #filename="graddesc_cellplot_comparison"
+    #truecell.mprimcell.plot(filename
+
+    #write down m and N_relative
+    mvsNfile = open(path_mvsN_output + "mvsN_run"+str(ID)+".txt", 'w+')
+    mvsNfile.writelines("["+str(N/total)+","+str(m)+"]")
+    mvsNfile.close()
+    print("Safed results of run " + str(ID))
+print("Safed all results succesfully")
